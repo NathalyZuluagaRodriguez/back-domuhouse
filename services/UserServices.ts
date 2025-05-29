ba7f4c7c2e0d5dd2f127306b8a2b01b2b44fee21
Mateo Henao <henaom0002@gmail.com>
2025-05-29 13:40:10 -0500

    cambios para el despliegue

--- services/UserServices.ts
+++ services/UserServices.ts
@@ -3,7 +3,7 @@ import generateHash from '../Helpers/generateHash';
 import UserRepository from '../repositories/UserRepository';
 import User from '../Dto/UserDto';
 import Login from '../Dto/loginDto';
-import Agent from '../Dto/AgentsDto';
+//import Agent from '../Dto/AgentsDto';
 
 
 class usuarioServi {
@@ -17,16 +17,16 @@ class usuarioServi {
         return await UserRepository.searchUser(login);
     }
 
-   static async registerAgent(agent: Agent) {
-        try {
-          console.log("Agente recibido en servicio:", agent);
-          agent.password = await generateHash(agent.password);
-          return await UserRepository.createAgente(agent);
-        } catch (error) {
-          console.error('Error al registrar agente:', error);
-          throw new Error('No se pudo registrar el agente');
-        }
-      }
+  // static async registerAgent(agent: Agent) {
+  //       try {
+  //         console.log("Agente recibido en servicio:", agent);
+  //         agent.password = await generateHash(agent.password);
+  //         return await UserRepository.createAgente(agent);
+  //       } catch (error) {
+  //         console.error('Error al registrar agente:', error);
+  //         throw new Error('No se pudo registrar el agente');
+  //       }
+  //     }
 
 }
 
