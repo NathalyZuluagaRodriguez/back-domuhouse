d4f1367344e92349437e3f0730015822f20f0d8a
Mariana314 <marianaocampomedina@gmail.com>
2025-06-26 15:05:45 -0500

    Creación ruta de obtener todos los token

--- repositories/invitacionRepository.ts
+++ repositories/invitacionRepository.ts
@@ -1,7 +1,32 @@
 // repositories/invitacionRepository.ts
 import db from '../config/config-db';
 
-export const saveInvitationToken = async (token: string, recipient_email: string, id_real_estate : string) => {
-  const query = 'INSERT INTO invitationtoken (token, recipient_email, id_real_estate) VALUES (?, ?, ?)';
-  await db.execute(query, [token, recipient_email,id_real_estate]);
-};
+export const saveInvitationToken = async (
+  token: string,
+  recipient_email: string,
+  realEstateId: number
+) => {
+  const sql = `
+    INSERT INTO invitationtoken
+      (token, recipient_email, id_real_estate, used, expires_at)
+    VALUES (?, ?, ?, 0, DATE_ADD(NOW(), INTERVAL 1 DAY));
+  `
+  await db.execute(sql, [token, recipient_email, realEstateId])
+}
+
+/* NUEVA FUNCIÓN */
+export const getAllInvitationTokens = async () => {
+  const sql = `
+    SELECT
+      token_id,
+      token,
+      recipient_email,
+      id_real_estate,
+      used,
+      created_at
+    FROM invitationtoken
+    ORDER BY created_at DESC;
+  `
+  const [rows] = await db.execute(sql)
+  return rows as any[]                       
+}
